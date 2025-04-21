0cb283c5c1fc38ef2f54083794ab77783d7ade45
頔珞JaderoChan <c_dl_cn@outlook.com>
2025-04-21 20:25:46 +0800

    Update example code.

--- example/single_block_mcstructure_example.cpp
+++ example/single_block_mcstructure_example.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-#include <mcnbt/mcstructure.hpp>
+#include <mcnbt/be/mcstructure.hpp>
 
 int main()
 {
     // Create a single block structure of command block. (Bedrock Edition)
-    nbt::CommandBlockED bed("say hello world!", 20, true, true, false);
-    nbt::CommandBlockSD bsd(false, nbt::CommandBlockSD::FD_UP);
+    nbt::CommandBlockBED bed("say hello world!", 20, true, true, false);
+    nbt::CommandBlockBSD bsd(false, nbt::CommandBlockBSD::FD_UP);
     auto structure = nbt::createSingleBlockStructure("minecraft:command_block", bed, bsd);
 
     // Save the structure to a file. (Bedrock Edition, Little Endian)
