19d9648b2ef7b3abea876392c57ceef2f7640ea1
頔珞JaderoChan <c_dl_cn@outlook.com>
2025-04-26 18:13:12 +0800

    增加be枚举项。

---
+++ include/mcnbt/be/enums/color.hpp
@@ -0,0 +1,34 @@
+#ifndef MCNBT_BE_ENUMS_COLOR_HPP
+#define MCNBT_BE_ENUMS_COLOR_HPP
+
+namespace nbt
+{
+
+namespace be
+{
+
+enum Color
+{
+    COLOR_BLACK,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_BROWN,
+    COLOR_BLUE,
+    COLOR_PURPLE,
+    COLOR_CYAN,
+    COLOR_LIGHT_GRAY,
+    COLOR_GRAY,
+    COLOR_PINK,
+    COLOR_LIME,
+    COLOR_YELLOW,
+    COLOR_LIGHT_BLUE,
+    COLOR_MAGENTA,
+    COLOR_ORANGE,
+    COLOR_WHITE
+};
+
+} // namespace be
+
+} // namespace nbt
+
+#endif // !MCNBT_BE_ENUMS_COLOR_HPP
